9b392adc95ce41e7693cf5bbe795b79dd1e115aa
terenzzzz <terence592592@gmail.com>
2022-09-07 09:54:56 +0100

    过渡

--- src/App.vue
+++ src/App.vue
@@ -14,25 +14,12 @@ export default {
 };
 </script>
 <style scoped>
-.slide-fade {
-  position: absolute;
-  left: 0;
-  right: 0;
-}
-
 .slide-fade-enter-active {
   transition: all .4s ease;
 }
 
-.slide-fade-leave-active {
-  transition: all .1s cubic-bezier(2.0, 0.5, 0.8, 1.0);
-}
-
-.slide-fade-enter,
-.slide-fade-leave-to {
-  left: 0;
-  right: 0;
-  transform: translateX(20px);
+.slide-fade-enter {
+  transform: translateX(15px);
   opacity: 0;
 }
 </style>
