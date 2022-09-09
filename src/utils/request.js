99075a6c0bf04c9e59719d8b7b4148c1ee823048
terenzzzz <terence592592@gmail.com>
2022-09-09 14:30:28 +0100

    change url

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,6 +2,7 @@
 import axios from 'axios'
 
 // axios.defaults.baseURL = "https://xs47628438.oicp.vip"
-axios.defaults.baseURL = "http://127.0.0.1:3007"
+// axios.defaults.baseURL = "http://127.0.0.1:3007"
+axios.defaults.baseURL = "https://eleme-node.herokuapp.com"
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 export default axios
