17c72604df71ca565a55151f14313a70937e155e
terenzzzz <terence592592@gmail.com>
2022-09-09 14:06:01 +0100

    1

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,7 @@
 // 网络请求封装
 import axios from 'axios'
-axios.defaults.baseURL = "https://xs47628438.oicp.vip"
+
+// axios.defaults.baseURL = "https://xs47628438.oicp.vip"
+axios.defaults.baseURL = "http://127.0.0.1:3007"
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 export default axios
--- src/views/Order/index.vue
+++ src/views/Order/index.vue
@@ -38,7 +38,7 @@ export default {
         };
     },
     components: { SingleOrder },
-    
+
 };
 </script>
 <style scoped>
