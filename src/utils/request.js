57d9f03f816c95b7ac02f25abeb9f3f965324d43
Terenzzzz <terence592592@gmail.com>
2022-12-29 18:47:30 +0000

    change host

--- src/utils/request.js
+++ src/utils/request.js
@@ -8,6 +8,6 @@ import axios from 'axios'
 // axios.defaults.baseURL = "http://127.0.0.1:3007"
 
 // 部署使用
-axios.defaults.baseURL = "http://46.101.60.239:3000"
+axios.defaults.baseURL = "https://www.terenzzzz.com:7777"
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 export default axios
